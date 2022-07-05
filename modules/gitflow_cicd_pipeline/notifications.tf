6410a370862c53b2fef1071eae90c5021480fb0b
Milan Simonovic <mbsimonovic@gmail.com>
2022-07-05 09:57:09 +0200

    notifications for both pipelines

--- modules/gitflow_cicd_pipeline/notifications.tf
+++ modules/gitflow_cicd_pipeline/notifications.tf
@@ -25,6 +25,33 @@ resource "aws_cloudwatch_event_target" "dev_target" {
   arn       = data.aws_ssm_parameter.sns_topic_arn.value
 }
 
+resource "aws_cloudwatch_event_rule" "release_rule" {
+  count       = var.enable_status_notifications ? 1 : 0
+  name        = "${var.project_name}-${var.github_branch_release}-cicd-pipeline"
+  description = "${var.project_name}-${var.github_branch_release} CodePipeline execution status"
+
+  event_pattern = <<PATTERN
+{
+    "detail-type": ["CodePipeline Action Execution State Change"],
+    "source": ["aws.codepipeline"],
+    "detail": {
+        "pipeline": ["${aws_codepipeline.pipeline_release.name}"],
+        "state":["SUCCEEDED","FAILED"],
+        "type": {
+          "category": ["Source","Deploy","Build","Test"]
+        }
+    }
+}
+PATTERN
+}
+
+resource "aws_cloudwatch_event_target" "relase_target" {
+  count     = var.enable_status_notifications ? 1 : 0
+  rule      = aws_cloudwatch_event_rule.release_rule[0].name
+  target_id = "SendToSNS"
+  arn       = data.aws_ssm_parameter.sns_topic_arn.value
+}
+
 data "aws_ssm_parameter" "sns_topic_arn" {
   name = "/shared/sns_cicd_alert_topic_arn"
 }
