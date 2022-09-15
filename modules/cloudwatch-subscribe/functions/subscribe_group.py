424a0d20aa910e7501d1b14d7925b5c420b38ef7
Milan Simonovic <mbsimonovic@gmail.com>
2022-09-15 09:37:55 +0200

    monitor logs for just 3 services
    the rest is covered with other monitoring tools

--- modules/cloudwatch-subscribe/functions/subscribe_group.py
+++ modules/cloudwatch-subscribe/functions/subscribe_group.py
@@ -13,6 +13,27 @@ import boto3
 
 cloudwatch_logs = boto3.client('logs')
 
+
+def should_subscribe(log_group_name: str) -> bool:
+    """
+    Check if log group should be subscribed
+
+    :param log_group_name: name of log group
+    :returns: boolean
+    """
+    if log_group_name.startswith("/aws/lambda/"):
+        return True
+    if log_group_name.startswith("API-Gateway-Execution-Logs"):
+        # this might be redundant, but just in case
+        return True
+    if log_group_name.startswith("/aws/elasticbeanstalk") and "/containers/" in log_group_name:
+        # this cover just the service logs, all other docker logs are excluded:
+        # /aws/elasticbeanstalk/mentorpal-dev-sbert/var/log/* are excluded
+        # /aws/elasticbeanstalk/mentorpal-*-sbert/containers/sbert-service.log are included
+        return True
+    return False
+
+
 def lambda_handler(event: Dict[str, Any], context: Dict[str, Any]) -> str:
     """
     Lambda function to subscribe newly created log groups
@@ -25,9 +46,12 @@ def lambda_handler(event: Dict[str, Any], context: Dict[str, Any]) -> str:
         print(f"Event logging enabled: `{json.dumps(event)}`")
 
     log_group_to_subscribe = event['detail']['requestParameters']['logGroupName']
-    if "controltower" in log_group_to_subscribe.lower():
+    print(f"new log group created: `{log_group_to_subscribe}")
+
+    if not should_subscribe(log_group_to_subscribe.lower()):
         print(f"Skipping subscription for AWS managed log group: `{log_group_to_subscribe}`")
         return
+
     print(f"Subscribing new log group `{log_group_to_subscribe}")
 
     cloudwatch_logs.put_subscription_filter(
